e92b6392ecdf37cc6b0f300e7d556948b9abf638
taylour <trm@inspire.ca>
2019-10-16 22:53:37 -0700

    Adds tutorials

--- src/components/tutorials.js
+++ src/components/tutorials.js
@@ -1,14 +1,58 @@
 import React from "react"
 // import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
-const Tutorials = ({ }) => {
+const TutorialItem = ({data}) => {
+  return (
+    <div style={{
+      margin: '15px',
+      padding: '5px',
+      background: '#EAEAEA',
+    }}>
+      <a href={data.url}>
+        <h3>{data.name}</h3>
+      </a>
+      <p>{data.description}</p>
+      <p>
+        Submitted by <a href={`https://github.com/${data.submitted_by.github}`}>{data.submitted_by.name}</a>
+      </p>
+    </div>
+  )
+}
+
+const Tutorials = () => {
+  const { allTutorialsJson } = useStaticQuery(
+    graphql`
+      query {
+        allTutorialsJson {
+          edges {
+            node {
+              id
+              name
+              description
+              url
+              submitted_by {
+                name
+                github
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const tutorials = allTutorialsJson.edges
 
   return (
     <div>
       <h1>Tutorials</h1>
       <p>Sometimes we just need a walkthrough! Here are some articles, videos, and tutorial series that you might find helpful.</p>
-      <em>These are coming soon...</em>
+      <ul>
+      {
+        tutorials.map(b => <TutorialItem key={b.node.id} data={b.node} />)
+      }
+      </ul>
     </div>
   )
 }
