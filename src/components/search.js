91cf1f92b3abab66be569f8b5bb6601b28143376
taylour <trm@inspire.ca>
2019-10-22 23:44:38 -0700

    Fixes spacing

--- src/components/search.js
+++ src/components/search.js
@@ -9,7 +9,7 @@ const Search = ({ setSearch }) => {
       }}
     >
       <input
-      placeholder="Search by tag"
+        placeholder="Search by tag"
         style={{
           marginLeft: '15px',
         }}
